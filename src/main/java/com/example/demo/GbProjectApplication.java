5c019a66f55c277c6ee840065a1e46c7862a4c96
손님 <@DESKTOP-KT3U08P>
2024-11-27 14:32:12 +0900

    project 업로드

---
+++ src/main/java/com/example/demo/GbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbProjectApplication.class, args);
+	}
+
+}
